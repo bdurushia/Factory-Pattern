d87122794a1fd17da7eaf149fe83e5725659cf02
bdurushia <b.durushia@gmail.com>
2024-03-06 12:29:37 -0600

    Completed Factory Pattern Exercise.

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Choose a car or motorcycle, enter how many wheels you want: ");
+            
+            int userChoice = 0;
+            bool isNumber = false;
+
+            while (!isNumber)
+            {
+                isNumber = int.TryParse(Console.ReadLine(), out userChoice);
+
+                if (userChoice != 2 && userChoice != 3 && userChoice != 4) 
+                {
+                    isNumber = false;
+                }
+                else
+                {
+                    isNumber = true;
+                }
+
+                if (isNumber == false)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Choose a car or motorcycle, enter how many wheels you want: ");
+                    Console.WriteLine("Please enter a valid number choice! Choose 2, 3, or 4.");
+                }
+            }
+
+            var userVehicle = VehicleFactory.GetVehicle(userChoice);
+
+            userVehicle.Drive();
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int userChoice)
+        {
+            switch  (userChoice)
+            {
+                case 4:
+                    return new Car();
+                case 3:
+                    return new Slingshot();
+                case 2:
+                    return new Motorcycle();
+                default:
+                    Console.WriteLine("Invalid number of wheels, default build:");
+                    return new Car();
+            }
+        }
+    }
+}
